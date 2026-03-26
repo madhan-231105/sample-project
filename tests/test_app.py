301330e2de622611fd169c34e21a45d64e65219c
madhan-231105 <mathangopi23@gmail.com>
2026-03-26 09:54:04 +0530

    Fix pytest import path

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add root directory to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import add, subtract
 
 def test_add():
